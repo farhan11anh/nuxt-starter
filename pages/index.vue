9f18b8cb60ac337e094dfe5187062f6803d4aa00
farhan11anh <farhan.fcbar@gmail.com>
2023-10-03 10:48:10 +0700

    navbar starterpack

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>
-    <NuxtDemo />
-  </div>
+    <div>
+        <nav class=" flex w-full bg-blend-hard-light flex-justify-between" >
+            <div class="grow-3" >
+                 
+            </div>
+            <ul class="flex gap-5 grow-2" >
+                <li>
+                    <NuxtLink to="#" >
+                        Home
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" >
+                        Information
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" >
+                        About
+                    </NuxtLink>
+                </li>
+            </ul>
+            <div class="grow-3" ></div>
+        </nav>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
 
-<style scoped></style>
+</script>
+
+<style scoped>
+
+</style>
